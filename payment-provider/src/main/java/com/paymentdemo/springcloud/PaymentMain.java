167416de548ca99a1bda6c120a743a8b1590c67d
Sebastian <2573992956@qq.com>
2020-07-06 00:59:05 +0800

    Initial commit

---
+++ payment-provider/src/main/java/com/paymentdemo/springcloud/PaymentMain.java
@@ -0,0 +1,15 @@
+package com.paymentdemo.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sebastian
+ * @date 7/6/20 12:55 AM
+ */
+@SpringBootApplication
+public class PaymentMain {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain.class, args);
+    }
+}
